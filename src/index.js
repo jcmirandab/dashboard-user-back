4c355a1658105f464dba4d74581858209fadf9e2
Juan Carlos <jcmirandab@gmail.com>
2023-09-06 16:45:54 -0300

    inicia servidor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 //TRAER EXPRESS
+const express = require('express')
 
 //INICIALIZAR EXPRESS
+const app = express()
 
 //AJUSTES***
 
@@ -9,3 +11,6 @@
 //RUTAS
 
 //PONER EL SERVIDOR A ESCUCHAR
+app.listen(3000, () => {
+    console.log(`Server started on port ${3000}`)
+})
